fe35d622731cc59315bfa6962a4c66c67eef2e4f
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-11-29 18:27:30 +0000

    Update Vincent Added

--- src/Vincent_part_3.js
+++ src/Vincent_part_3.js
@@ -4,7 +4,16 @@ import React from "react";
 
 function Vincent_part_3() {
     return(
-        <div></div>
+
+            <div id="part3">
+                <h2>Alter</h2>
+                <div className="d-flex justify-content-center">
+                    <button id="game-button">Try the game</button>
+                </div>
+            </div>
+
+
+
     )
 
 }
