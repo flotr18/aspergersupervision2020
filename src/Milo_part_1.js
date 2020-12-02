e7b17a522d154c8a14a563e041e116b427995b32
Archibaald-dev <milo.lallement@yahoo.fr>
2020-12-02 14:42:59 +0100

    oe

--- src/Milo_part_1.js
+++ src/Milo_part_1.js
@@ -3,6 +3,9 @@ import React from 'react';
 function Milo_part_1() {
     return (
         <div className="header">
+            <video autoplay muted loop>
+                <source src="https://www.youtube.com/watch?v=lRTtMcx6rSM" type="video/mp4"/>
+            </video>
             <div className="extend"></div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iste animi eligendi aspernatur natus est et
                 obcaecati dignissimos deserunt, facilis maxime voluptate praesentium. Assumenda eaque corrupti quasi eum
