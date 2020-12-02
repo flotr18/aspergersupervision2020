eb77e59cd7c5d7667d8a9b3dd3558a1cafed9410
Julien QUENNEHEN <julien.quennehen@edu.devinci.fr>
2020-12-02 17:40:28 +0000

    fix partners à revoir avec les images

--- src/Julien_part_4.js
+++ src/Julien_part_4.js
@@ -1,26 +1,26 @@
-import './css/style_partners.css'
+import "./css/style_partners.css";
 import React from "react";
 
-
 function Julien_part_4() {
-    return (
-        <div>
-            <p className="ourPartners">Our Partners</p>
-            <p className="Paragraph">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the
-                1500s, when an unknown printer took a galley of type and scrambled it to
-                make a type specimen book.
-            </p>
-            <div className="partner">
-                <div className="foo blue"></div>
-                <div className="foo orange"></div>
-                <div className="foo green"></div>
-                <div className="foo yellow"></div>
-            </div>
+  return (
+    <div>
+      <p className="ourPartners">Our Partners</p>
+      <p className="Paragraph">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-3 foo blue"></div>
+          <div className="col-3 foo orange"></div>
+          <div className="col-3 foo green"></div>
+          <div className="col-3 foo yellow"></div>
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
 
-export default Julien_part_4
+export default Julien_part_4;
