451ac592e4d228a24e7bc7ae66cd49dbeda106d6
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-12-02 16:52:56 +0100

    Update Eliott Part finish
    Eliott part finish (may have some final finitions)

--- src/Eliott_part_2.js
+++ src/Eliott_part_2.js
@@ -1,30 +1,31 @@
 import React from "react";
+import './css/style_block.css'
 
 function Eliott_part_2() {
     return (
         <div>
             <h1 className="text-center"><font face="GoodDog New">A new way to learn about the syndrome</font></h1>
-            <div>
-                <div>
-                    <p className=""><font face="GoodDog New">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat.</font></p>
+
+            <div className="block">
+
+                <div className="row">
+                    <div className="col-6">
+                        <span className="color-block-a"></span>
+                    </div>
+                    <div className="col-6">
+                        <p className="p1">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
                 </div>
-                <div>
-                    <img className="" src="" alt=""/>
+
+                <div className="row">
+                    <div className="col-6">
+                        <p className="p2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <div className="col-6">
+                        <span className="color-block-b"></span>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <img className="" src="" alt=""/>
-                </div>
-                <div>
-                    <p className=""><font face="GoodDog New">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat.</font></p>
-                </div>
+
             </div>
         </div>
     )
