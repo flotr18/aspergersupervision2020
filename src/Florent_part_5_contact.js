0dfbdbaedf5ffc8e1b2984902d9d708ba872f0bd
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-12-04 00:18:16 +0000

    Update Form

--- src/Florent_part_5_contact.js
+++ src/Florent_part_5_contact.js
@@ -5,25 +5,42 @@ function Florent_part_5_contact() {
         <div>
             <p className="form_title h1 text-center">CONTACT US</p>
 
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12 mx-auto">
-                        <div className="form_contact col-8 mx-auto">
-                            <div className="row">
-                                <div className="test col-6">
-                                </div>
-                                <div className="test2 col-6">
-                                </div>
-                                <div className="test3 col-12">
-                                </div>
-                                <div className="test4 col-12">
-                                </div>
-                                <div className="test5 col-12">
-                                </div>
-                            </div>
-                        </div>
+            <div className="container-fluid text-center w-75 form_contact">
+
+                <div className="row mt-3">
+                <div className="col-6 firstname">
+                    <input className={"contact_input w-50 mt-5"} placeholder={"first name"} />
+
+                </div>
+                <div className="col-6 lastname">
+                    <input className={"contact_input w-50 mt-5"} placeholder={"first name"} />
+
+                </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-12 email">
+                        <input className={"contact_input w-75 mt-3"} placeholder={"first name"} />
+
                     </div>
                 </div>
+                <div className="row mt-5">
+                    <div className="col-12 message">
+                        <input className={"contact_input w-75 mt-3"} placeholder={"first name"} />
+
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-12 submit">
+                        <input className={"contact_input w-75 mt-3"} placeholder={"first name"} />
+
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-12 disclaimer">
+
+                    </div>
+                </div>
+
             </div>
         </div>
     )
