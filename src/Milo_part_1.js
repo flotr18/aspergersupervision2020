06e64b2588cb1d9d2cc3bcf4a8e259703e7f431b
Archibaald-dev <milo.lallement@yahoo.fr>
2020-12-09 15:56:24 +0100

    img + responsive

--- src/Milo_part_1.js
+++ src/Milo_part_1.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import neon from './css/img/neon.mp4';
+import img from './css/img/Plan_de_travail_2.jpg';
 
 function Milo_part_1() {
     const videoSource = "./img_videos/neon.mp4"
     return (
         <div className="header">
-            <video className='videoTag' id="video" autoPlay loop muted>
-                <source src={neon} type='video/mp4' />
-            </video>
-            <div id="videoMessage" className="styling">
-                <div className="extend"></div>
+            <img src={img} id="img" alt="presentation img"/>
+            <div id="imgMessage" className="styling">
                 <p>Discover Alter, a serious game in which you will understand the difficulty of someone being affected by the Asperger syndrome. Try to communicate and befriend all the people by making the good choices
 </p>
                 <button>Discover the game</button>
@@ -19,5 +17,10 @@ function Milo_part_1() {
     )
 
 }
+/*
+<video className='videoTag' id="video" autoPlay loop muted>
+                <source src={neon} type='video/mp4' />
+            </video>
+*/
 
 export default Milo_part_1
