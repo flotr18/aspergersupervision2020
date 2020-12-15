cff76ca79f97e7c4103326c7d6aef9321fa7be06
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-12-15 06:29:46 +0000

    Update

--- src/Florent_part_5_contact.js
+++ src/Florent_part_5_contact.js
@@ -105,7 +105,13 @@ class Florent_part_5_contact extends Component {
                     </div>
                 </form>
 
-
+                <div className={"container-fluid text-center w-75"}>
+                    <div className={"row"}>
+                        <div className={"col disclaimer"}>
+                            <p className={"h6 disclaimer-message"}>DISCLAIMER/MESSAGE</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
