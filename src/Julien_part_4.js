2c507092fbd76482567a7e8f1c654075731c6cfa
Julien QUENNEHEN <55101915+JulienQHN@users.noreply.github.com>
2020-12-16 14:24:24 +0100

    Update Julien_part_4.js
    Update Paragraph text of "Julien_part_4.js"

--- src/Julien_part_4.js
+++ src/Julien_part_4.js
@@ -8,21 +8,15 @@ function Julien_part_4() {
     <div>
       <p className="ourPartners">Our Partners</p>
       <p className="Paragraph">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
-      </p>
+          We would like to express a special thanks to Autism Society and Aspire
+          who accompanied us in this project and provided us all what we needed
+          to reach the finality of this serious game.
+        </p>
       <div className="container">
         <div className="row">
           <img className="col-6 foo" src={block1}></img>
           <img className="col-6 foo" src={block2}></img>
         </div>
-        <p className="Paragraph">
-          We would like to express a special thanks to Autism Society and Aspire
-          who accompanied us in this project and provided us all what we needed
-          to reach the finality of this serious game.
-        </p>
       </div>
     </div>
   );
